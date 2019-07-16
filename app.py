19a13c983640ed2e2308913987d6c656dc8c6de9
Paolino Iorio <paolino.iorio@eni.com>
2019-07-16 17:34:50 +0200

    title first commit

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    index = open("index.html").read().format(first_header='goodbye',p1='World',p2='Hello')
-    return render_template(index)
+    animal = 'dog'
+    return render_template("index.html", value=animal)
 
 
 
