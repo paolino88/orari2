e70c49fa0599976202c163dc84457b6586ac38e2
Paolino Iorio <paolino.iorio@eni.com>
2019-07-18 17:43:42 +0200

    title first commit

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask, render_template
-from tabula import read_pdf
-import urllib.request
-import pandas as pd
-import numpy as np
-import datetime
-import re
-import os
+
 
 app = Flask(__name__)
 
